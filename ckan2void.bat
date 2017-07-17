330ba7b1a860b88d4f341fb09725b958ed327943
Luiz Leme <lapaesleme@gmail.com>
2017-07-17 11:47:38 -0300

    ok

--- ckan2void.bat
+++ ckan2void.bat
@@ -1,3 +1,4 @@
-mvn clean install
-cd target
-java -cp ckan2void-1.0.0-SNAPSHOT.jar;dependency/* Main
+@echo off
+call mvn clean install
+cd .\target
+java -cp ckan2void-1.0.0-SNAPSHOT.jar;dependency\* Main %1

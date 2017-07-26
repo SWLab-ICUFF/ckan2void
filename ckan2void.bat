d5600e1a6d228447610bcbba1e7f68c146ba5fca
lapaesleme <lapaesleme@gmail.com>
2017-07-26 19:16:51 -0300

    ok

--- ckan2void.bat
+++ ckan2void.bat
@@ -1,5 +1,5 @@
 @echo off
 call mvn clean install
 cd .\target
-java -Xmx4096m -cp ckan2void-1.0.0-SNAPSHOT.jar;dependency\* Main
+java -Xmx2048m -cp ckan2void-1.0.0-SNAPSHOT.jar;dependency\* Main
 cd ..
